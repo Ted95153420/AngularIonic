e7b2ddd022dfd6007b48a1bb481001c6fcc093be
Ted Milne <tmilne@visualstatement.com>
2015-12-04 11:29:44 -0800

    changed layout of returnapiresultservice so that it reads better to me. getbookdetails clearly invoves a function that returns a promise

--- Dev/controllers/mycontrollers.js
+++ Dev/controllers/mycontrollers.js
@@ -2,32 +2,6 @@ angular.module('f1driverapp.controllers',[]).
 	controller('drivercontroller', function($scope, returnapiresultservice) {
 	
 		$scope.hardcodedbooktitle = "Harry Potter";
-		$scope.stufffromservice = returnapiresultservice.getDrivers();
+		$scope.stufffromservice = returnapiresultservice.getbookdetails();
 });
-
-   
-  /*$scope.driversList = [
-      {
-          Driver: {
-              givenName: 'Sebastian',
-              familyName: 'Vettel'
-          },
-          points: 322,
-          nationality: "German",
-          Constructors: [
-              {name: "Red Bull"}
-          ]
-      },
-      {
-          Driver: {
-          givenName: 'Fernando',
-              familyName: 'Alonso'
-          },
-          points: 207,
-          nationality: "Spanish",
-          Constructors: [
-              {name: "Ferrari"}
-          ]
-      }
-    ]; 
-}]); */
+ 
--- Dev/controllers/services.js
+++ Dev/controllers/services.js
@@ -1,22 +1,22 @@
-angular.module('f1driverapp.services', []).
-  factory('returnapiresultservice', function($http) {
-
-    var testingAPI = {};
-
-    testingAPI.getDrivers = function() {
-       //Just use the API to get any old book.
-	   return $http.get('https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699').then
-		(function(response)
+angular.module('f1driverapp.services',[]).factory('returnapiresultservice', function($http) {
+    return {
+		getbookdetails: function() 
 		{
-			alert(YES);
-			//return response.data;
-		},
-		function(error)
-		{
-			//return 'something went wrong';
-			alerd("NOPE");
-		});
+            var bookDetailPromise =  $http.get('https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699')
+            .then(function(response) 
+			{
+				alert("YES");
+                //return response.data;
+            },
+			function(error)
+			{
+				alert("NOPE");
+				//eturn 'NOPE';
+			}
+			);
+            // Return the promise to the controller
+            return bookDetailPromise;
+        }
     }
-
-    return testingAPI;
-  });
+});
+	
